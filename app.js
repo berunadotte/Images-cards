c62138b6ecb84e32b7db52185c2acede3e3b1227
berunadotte <41091437+berunadotte@users.noreply.github.com>
2022-06-19 01:37:47 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
-const upBtn = document.querySelector('.up-button')
-const downBtn = document.querySelector('.down-button')
+const slides = document.querySelectorAll('.slide')
 
-const sidebar = document.querySelector('.sidebar')
+for (const slide of slides) {
+  slide.addEventListener('click', () => {
+    clearActiveClasses()
 
-const container = document.querySelector('.container')
-const mainSlide = document.querySelector('.main-slide')
-const slidesCount = mainSlide.querySelectorAll('div').length
+    slide.classList.add('active')
+  })
+}
 
-let activeSlideIndex = 0
-
-sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
-
-upBtn.addEventListener('click', () => {
-  changeSlide('up')
-})
-
-downBtn.addEventListener('click', () => {
-  changeSlide('down')
-})
-
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'ArrowUp') {
-    changeSlide('up')
-  } else if (event.key === 'ArrowDown') {
-    changeSlide('down')
-  }
-})
-
-function changeSlide(direction) {
-  if (direction === 'up') {
-    activeSlideIndex++
-    if (activeSlideIndex === slidesCount) {
-      activeSlideIndex = 0
-    }
-  } else if (direction === 'down') {
-    activeSlideIndex--
-    if (activeSlideIndex < 0) {
-      activeSlideIndex = slidesCount - 1
-    }
-  }
-
-  const height = container.clientHeight
-  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
-  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+function clearActiveClasses() {
+  slides.forEach((slide) => {
+    slide.classList.remove('active')
+  })
 }
